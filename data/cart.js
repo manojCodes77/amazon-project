8c2325095261be6b293e7cb25c34217c40a44c36
Manoj Singh Rawat <natkhatkanharawatji@gmail.com>
2024-05-27 12:03:34 +0530

    create automated test for add to cart

--- data/cart.js
+++ data/cart.js
@@ -1,23 +1,28 @@
 // this file contains all the code that is required to add the products to the cart page and make the add to cart button work.
 
 // here i want that the cart variable should be available to all the files that import this file.
-export let cart = JSON.parse(localStorage.getItem("cart"));
+export let cart;
 
-if (!cart) {
-  cart = [
-    {
-      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity: 2,
-      deliveryOptionId: "1",
-    },
-    {
-      productId: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
-      quantity: 1,
-      deliveryOptionId: "2",
-    }
-    // these are the sample objects that are added to the cart array. using javascript
-  ];
-}
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem("cart"));
+  if (!cart) {
+    cart = [
+      {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+        deliveryOptionId: "1",
+      },
+      {
+        productId: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+        quantity: 1,
+        deliveryOptionId: "2",
+      }
+      // these are the sample objects that are added to the cart array. using javascript
+    ];
+  }
+};
 
 function saveToStorage() {
   localStorage.setItem("cart", JSON.stringify(cart));
@@ -45,7 +50,6 @@ export function addToCart(productId) {
   };
 
   saveToStorage();
-
 }
 
 export function removeFromCart(productId) {
--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -1,33 +1,43 @@
-import {addToCart} from '../../data/cart.js';
+import {addToCart,cart,loadFromStorage} from '../../data/cart.js';
 
 describe('test suite: addToCart', () => {
-    it('adds an item to the cart',()=>{
-        let cart = [];
-        addToCart(cart, {id:1, name: 'Laptop', price: 1000});
+    it('adds an existing product to the cart', () => {
+        spyOn(localStorage,'setItem');
+        spyOn(localStorage,'getItem').and.callFake(()=>
+        {
+            return JSON.stringify([
+                {
+                    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                    quantity: 1,
+                    deliveryOptionId: '1',
+                }
+            ]);
+            
+        });
+        console.log(localStorage.getItem("cart"));
+        loadFromStorage();
+
+        addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
         expect(cart.length).toEqual(1);
-    });
-
-    it('adds the correct item to the cart',()=>{
-        let cart = [];
-        addToCart(cart, {id:1, name: 'Laptop', price: 1000});
-        expect(cart[0].name).toEqual('Laptop');
-    });
-
-    it('adds the correct price to the cart',()=>{
-        let cart = [];
-        addToCart(cart, {id:1, name: 'Laptop', price: 1000});
-        expect(cart[0].price).toEqual(1000);
-    });
-
-    it('adds the correct quantity to the cart',()=>{
-        let cart = [];
-        addToCart(cart, {id:1, name: 'Laptop', price: 1000});
-        expect(cart[0].quantity).toEqual(1);
-    });
-
-    it('increments the quantity if the item is already in the cart',()=>{
-        let cart = [{id:1, name: 'Laptop', price: 1000, quantity: 1}];
-        addToCart(cart, {id:1, name: 'Laptop', price: 1000});
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
         expect(cart[0].quantity).toEqual(2);
+    });
+
+    it('adds a new product to the cart', () => {
+        spyOn(localStorage,'setItem');
+
+        spyOn(localStorage,'getItem').and.callFake(()=>
+        {
+            return JSON.stringify([]);
+        });
+        console.log(localStorage.getItem("cart"));
+        loadFromStorage();
+
+        addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+        expect(cart.length).toEqual(1);
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+        expect(cart[0].quantity).toEqual(1);
     });
 });
